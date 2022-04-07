631ab57c4decd1c27c7081344070df0dd11e2d22
Kyle Curry <kylecurry43@gmail.com>
2022-04-06 20:52:07 -0500

    removed unused imports

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import "./firebase";
-import { NavigationContainer } from '@react-navigation/native';
 import MainFlow from './src/routes/MainFlow';
-import AuthFlow from './src/routes/AuthFlow';
 import { Provider as AuthProvider } from './src/context/authContext';
 
 
